451da84e1c7fb3818909db3f890d70944b4237c5
Joy Timmermans <joy.timmermans@gmail.com>
2021-03-16 19:29:23 +0100

    added unittest

---
+++ test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from Board import Board
+from Testboards import Testboards as TB
+
+class Testboard(unittest.TestCase):
+
+    def test_knightTesting(self):
+        board = Board()
+        board.board=TB.knightmoverTest
+        self.assertEqual("[2:4, 3:5, 5:5, 6:4, 6:2, 5:1, 3:1, 2:2]", str(board.getPossibleMoves(4,3)))
+
+
+    def test_RookTesting(self):
+        board = Board()
+        board.board=TB.rookAccessTest
+        self.assertEqual("[5:3, 3:3, 2:3, 1:3, 4:4, 4:5, 4:6, 4:7, 4:2, 4:1, 4:0]", str(board.getPossibleMoves(4,3)))
+
+if __name__ == '__main__':
+    unittest.main()
