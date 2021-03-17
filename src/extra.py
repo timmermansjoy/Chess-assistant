afb9eee153f891f150b4e510872f6a342fb36008
Joy Timmermans <joy.timmermans@gmail.com>
2021-03-17 22:10:44 +0100

    testing github actions

--- src/extra.py
+++ src/extra.py
@@ -3,5 +3,6 @@ class Coordinate:
     def __init__(self, row, column):
         self.row = row
         self.column = column
+
     def __repr__(self):
         return str(self.row) + ":" + str(self.column)
