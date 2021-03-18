82beda2c3f5ec50ba62d4c2ddaf497162b38fdd8
Joy Timmermans <joy.timmermans@gmail.com>
2021-03-18 16:17:44 +0100

    added notationmove test and refactored tests using setup

--- src/test.py
+++ src/test.py
@@ -17,136 +17,124 @@ from Testboards import Testboards as TB
 # verify the length of "actual" (exactly 5 characters per possible moves)
 
 class Testboard(unittest.TestCase):
+
+    def setUp(self):
+        self.board = Board()
+
     # Knight moves
     # White Knight Moves
+
     def test_UnrestrictedWhiteKnight(self):
-        board = Board()
-        board.board = TB.BlankBoard
-        board.board[4][3] = "N"
-        actual = str(board.getPossibleMoves(4, 3))
+        self.board.board = TB.BlankBoard
+        self.board.board[4][3] = "N"
+        actual = str(self.board.getPossibleMoves(4, 3))
         results = ["2:2", "2:4", "3:5", "5:5", "6:4", "6:2", "5:1", "3:1"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(40, len(actual))
-        del board
 
     def test_CapturingWhiteKnight(self):
-        board = Board()
-        board.board = TB.BlankBoard2
-        board.board[4][3] = "N"
-        board.board[2][2] = 'n'
-        actual = str(board.getPossibleMoves(4, 3))
+        self.board.board = TB.BlankBoard2
+        self.board.board[4][3] = "N"
+        self.board.board[2][2] = 'n'
+        actual = str(self.board.getPossibleMoves(4, 3))
         results = ["2:2", "2:4", "3:5", "5:5", "6:4", "6:2", "5:1", "3:1"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(40, len(actual))
-        del board
 
     def test_RestrictedWhiteKnight(self):
-        board = Board()
-        board.board = TB.BlankBoard1
-        board.board[4][3] = "N"
-        board.board[2][2] = 'n'
-        board.board[2][4] = 'n'
-        board.board[3][5] = 'N'
-        board.board[5][5] = 'N'
-        actual = str(board.getPossibleMoves(4, 3))
+        self.board.board = TB.BlankBoard1
+        self.board.board[4][3] = "N"
+        self.board.board[2][2] = 'n'
+        self.board.board[2][4] = 'n'
+        self.board.board[3][5] = 'N'
+        self.board.board[5][5] = 'N'
+        actual = str(self.board.getPossibleMoves(4, 3))
         results = ["2:2", "2:4", "6:4", "6:2", "5:1", "3:1"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(30, len(actual))
-        del board
-        # Black Knight Moves
 
+    # Black Knight Moves
     def test_knightTesting(self):
-        board = Board()
-        board.board = TB.knightAccessTest
-        self.assertEqual("[2:4, 3:5, 5:5, 6:4, 6:2, 5:1, 3:1, 2:2]", str(board.getPossibleMoves(4, 3)))
-        del board
+        self.board.board = TB.knightAccessTest
+        self.assertEqual("[2:4, 3:5, 5:5, 6:4, 6:2, 5:1, 3:1, 2:2]", str(self.board.getPossibleMoves(4, 3)))
 
     def test_RookTesting(self):
-        board = Board()
-        board.board = TB.rookAccessTest
-        self.assertEqual("[5:3, 3:3, 2:3, 1:3, 4:4, 4:5, 4:6, 4:7, 4:2, 4:1, 4:0]", str(board.getPossibleMoves(4, 3)))
-        del board
+        self.board.board = TB.rookAccessTest
+        self.assertEqual("[5:3, 3:3, 2:3, 1:3, 4:4, 4:5, 4:6, 4:7, 4:2, 4:1, 4:0]", str(self.board.getPossibleMoves(4, 3)))
 
     def test_bishopTesting(self):
-        board = Board()
-        board.board = TB.bishopAccessTest
+        self.board.board = TB.bishopAccessTest
         results = ["3:4", "2:5", "5:2", "6:1", "5:4", "6:5", "3:2", "2:1"]
-        actual = str(board.getPossibleMoves(4, 3))
+        actual = str(self.board.getPossibleMoves(4, 3))
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(len(actual), 40)
-        del board
 
     # Pawn Moves
     def test_PawnMoves(self):
         board = Board()
-        self.assertEqual("[2:7, 3:7]", str(board.getPossibleMoves(1, 7)))
+        self.assertEqual("[2:7, 3:7]", str(self.board.getPossibleMoves(1, 7)))
         del board
 
         # White Pawn moves
 
     def test_WhitePawnNoTakeOptions(self):
-        board = Board()
-        actual = str(board.getPossibleMoves(6, 3))
+        actual = str(self.board.getPossibleMoves(6, 3))
         results = ["5:3", "4:3"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(10, len(actual))
-        del board
 
     def test_WhitePawnWithTakeOptions(self):
-        board = Board()
-        board.board = TB.TwoPawnTakeOptions
-        actual = str(board.getPossibleMoves(4, 3))
+        self.board.board = TB.TwoPawnTakeOptions
+        actual = str(self.board.getPossibleMoves(4, 3))
         results = ["3:2", "3:3", "3:4"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(15, len(actual))
-        del board
 
     def test_WhitePawnWithOneTakeOneMove(self):
-        board = Board()
-        board.board = TB.OneTakeOneMoveOption
-        actual = str(board.getPossibleMoves(4, 3))
+        self.board.board = TB.OneTakeOneMoveOption
+        actual = str(self.board.getPossibleMoves(4, 3))
         results = ["3:3", "3:4"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(10, len(actual))
-        del board
 
         # Black Pawn moves
 
     def test_BlackPawnNoTakeOptions(self):
-        board = Board()
-        actual = str(board.getPossibleMoves(1, 3))
+        actual = str(self.board.getPossibleMoves(1, 3))
         results = ["3:3", "2:3"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(10, len(actual))
-        del board
 
     def test_BlackPawnWithTwoTakeOptions(self):
-        board = Board()
-        board.board = TB.BlackTwoPawnTakeOptions
-        actual = str(board.getPossibleMoves(4, 3))
+        self.board.board = TB.BlackTwoPawnTakeOptions
+        actual = str(self.board.getPossibleMoves(4, 3))
         results = ["5:2", "5:3", "5:4"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(15, len(actual))
-        del board
 
     def test_BlackPawnWithOneTakeOneMove(self):
-        board = Board()
-        board.board = TB.BlackOneTakeOneMoveOption
-        actual = str(board.getPossibleMoves(4, 3))
+        self.board.board = TB.BlackOneTakeOneMoveOption
+        actual = str(self.board.getPossibleMoves(4, 3))
         results = ["5:3", "5:4"]
         for i in results:
             self.assertIn(i, actual)
         self.assertEqual(10, len(actual))
-        del board
+
+    def test_NotationMoveKnight(self):
+        self.assertEqual('N', self.board.board[7][1])
+        self.assertEqual('.', self.board.board[5][2])
+        self.board.notationMove("b1c3")
+        self.assertEqual('.', self.board.board[7][1])
+        self.assertEqual('N', self.board.board[5][2])
 
 
 if __name__ == '__main__':
