fd878b5423d7ff28b65263dda45819457b929f12
timmermansjoy <joy.timmermans@student.pxl.be>
2021-03-23 16:35:36 +0100

    fixed wierd formatting

--- src/main.py
+++ src/main.py
@@ -7,18 +7,11 @@ def main():
     # board.board = TB.BlackOneTakeOneMoveOption
     board.board = TB.kingMoves
     print(board)
-<<<<<<< HEAD
-    print(board.getPossibleMoves(3, 4))
-    board.board = TB.kingMoves
-    print(board.getAllAttackedFields(False))
-    print(board.getAllAttackedFields(True))
-=======
     print(board.getPossibleMoves(3, 0))
     board.board = TB.kingMoves
     print(board.getAllAttackedFields(False))
     print(board.getAllAttackedFields(True))
 
->>>>>>> 33380ece0c9f5e15920a0eb4d3a2c3516e6d03da
 
 if __name__ == "__main__":
     main()
