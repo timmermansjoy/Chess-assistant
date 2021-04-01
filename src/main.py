272001ddecf86f9c03e8f37619efc82959a7cc97
muhammedsenturkPXL <muhammed.senturk@student.pxl.be>
2021-04-01 13:16:43 +0200

    implemented isCheck() method

--- src/main.py
+++ src/main.py
@@ -4,14 +4,13 @@ from testboards import Testboards as TB
 
 def main():
     board = Board()
-    board.move(6, 4, 4, 4)
-    board.move(1, 4, 3, 4)
-    board.move(7, 5, 5, 3)
-    board.move(0, 6, 2, 5)
-    board.move(7, 6, 5, 5)
-    board.move(0, 5, 5, 0)
-    board.board[2][3] = "B"
-    board.castling(True, False)
+    board.move(6,7,5,7)
+    board.move(1,2,2,2)
+    board.move(6,4,5,4)
+    board.move(0,3,3,0)
+    print(board)
+    board.move(6,3,5,3)
+
     print(board.moveLog)
     print(board.GetChessNotation())
     # print(board.fieldsUnderWhiteThreat)
