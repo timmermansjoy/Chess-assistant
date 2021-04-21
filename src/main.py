c71a37355f07aaff02b4cf25213e6247b3d2a118
Muhammed Senturk <muhammed.senturk@student.pxl.be>
2021-04-21 04:31:39 +0200

    added functionality to evaluation function

--- src/main.py
+++ src/main.py
@@ -7,16 +7,18 @@ import ai
 def main():
     board = Board()
 
-    for i in range(11):
+    for i in range(2):
         if i%2 == 0:
             print("My Turn:")
             beginCoord, endCoord = ai.minimaxRoot(3,board,True)
             board.move(beginCoord.row, beginCoord.column, endCoord.row, endCoord.column)
+            print(board)
             
         else:
             print("Computers Turn:")
             beginCoord, endCoord = ai.minimaxRoot(3,board,False)
             board.move(beginCoord.row, beginCoord.column, endCoord.row, endCoord.column)
+            print(board)
             
         print(board)
 
