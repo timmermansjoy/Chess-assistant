68ca8be028ee299e74c203a48735750dc093882a
timmermansjoy <joy.timmermans@student.pxl.be>
2021-04-25 09:50:47 +0200

    added timer feature

--- src/main.py
+++ src/main.py
@@ -2,12 +2,14 @@ from board import *
 from testboards import Testboards as TB
 from extra import Coordinate
 import ai
+import timeit
 
 
 def main():
     board = Board()
+    for i in range(2):
+        start = timeit.default_timer()
 
-    for i in range(10):
         if i % 2 == 0:
             print("My Turn:")
             beginCoord, endCoord = ai.calculateMove(3, board, True, i)
@@ -19,6 +21,11 @@ def main():
             board.move(beginCoord.row, beginCoord.column, endCoord.row, endCoord.column)
 
         print(board)
+        # Your statements here
+
+        stop = timeit.default_timer()
+
+        print('Time: ', stop - start)
 
 
 if __name__ == "__main__":
