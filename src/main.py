ab24a6a6d3d0cc812b37ac065cfff161d92ab322
muhammedsenturkPXL <muhammed.senturk@student.pxl.be>
2021-05-04 13:58:37 +0200

    fixed pawnEvaluations and AI

--- src/main.py
+++ src/main.py
@@ -8,21 +8,23 @@ import timeit
 def main():
     board = Board()
 
-    for i in range(9):
+    for i in range(40):
         start = timeit.default_timer()
         if i % 2 == 0:
             print("My Turn:")
             beginCoord, endCoord = ai.calculateMove(3, board, True, i)
+            print("------------------------------------------------ initial move")
             board.move(beginCoord.row, beginCoord.column, endCoord.row, endCoord.column)
 
         else:
             print("Computers Turn:")
             beginCoord, endCoord = ai.calculateMove(3, board, False, i)
+            print("------------------------------------------------ initial move")
             board.move(beginCoord.row, beginCoord.column, endCoord.row, endCoord.column)
-
-        print(board)
+            
         # Your statements here
-
+        print(board)
+        print("current game state:", ai.evaluation(board))
         stop = timeit.default_timer()
 
         print('Time: ', stop - start)
