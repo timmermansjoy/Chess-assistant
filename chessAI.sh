edb2951d027a03e3c80bd4763809398c65061b0f
KarelSajdakPXL <11902600@student.pxl.be>
2021-05-04 16:56:15 +0200

    Added a shellscript that starts window.py etc.

---
+++ chessAI.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Set Session Name
+SESSION="chessAI"
+SESSIONEXISTS=$(tmux list-sessions | grep $SESSION)
+
+# Only create tmux session if it doesn't already exist
+if [ "$SESSIONEXISTS" = "" ]
+then
+    # Start New Session with our name
+    tmux new-session -d -s $SESSION
+    
+    # start window system
+    tmux new-window -t $SESSION:1 -n 'roscore'
+    tmux send-keys -t 'roscore' 'roscore' C-m
+
+    # start window system
+    tmux new-window -t $SESSION:2 -n 'rosbag'
+    tmux send-keys -t 'rosbag' 'cd ~/Data/bags' C-m
+    tmux send-keys -t 'rosbag' 'rosbag play -l demo.bag' C-m
+
+    # start window system
+    tmux new-window -t $SESSION:3 -n 'window'
+    tmux send-keys -t 'window' './src/abstraction/scripts/window.py ' C-m
+
+    tmux join-pane -v -s 2 -t 1
+    tmux join-pane -v -s 3 -t 1
+
+fi
+
+# Attach Session, on the Main window
+tmux attach-session -t $SESSION:1
