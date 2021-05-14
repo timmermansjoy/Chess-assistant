3ab26f5cac4c220e34ebd96ed128353066ab2f94
Nikki Bruls <Nikki.Bruls@student.pxl.be>
2021-05-14 21:23:00 +0200

    Squares are detected perfectly

--- src/abstraction/scripts/abstraction.py
+++ src/abstraction/scripts/abstraction.py
@@ -15,10 +15,12 @@ class abstraction:
         self.imageLock = Lock()
         self.bridge = CvBridge()
         self.rate = rospy.Rate(10)
-        self.GUI_UPDATE_PERIOD = 0.10
+        self.GUI_UPDATE_PERIOD = 0.7
         self.redrawTimer = rospy.Timer(rospy.Duration(self.GUI_UPDATE_PERIOD), self.callback_redraw)
-        
-        
+        self.corners = None
+        self.previous_images = []
+        self.max_length = 10
+
         # ---- Subscribers ----
         self.chesscamsubscriber = rospy.Subscriber('/chesscam/compressed', Image, self.callback_chesscam)
         rospy.loginfo('subscribed to /chesscam/compressed')
@@ -35,16 +37,42 @@ class abstraction:
                 image_cv = self.convert_ros_to_opencv(self.image)
             finally:
                 self.imageLock.release()
-            
-            image_cv = cv2.resize(image_cv, (1069, 599))
-            
-            img = cv_utils.magic(image_cv)
 
+            image_cv = cv2.resize(image_cv, (1069, 599))
+
+            corners = cv_utils.calculate_corners(image_cv)
+            if corners is not None:
+                self.corners = corners
+            # img = cv_utils.get_squares(corners, image_cv)
+            #self.move_piece(image_cv)
+            #img = cv_utils.detect_and_draw_circles(image_cv)
+            img = image_cv
+            if self.corners is not None:
+                img = cv_utils.get_squares(self.corners,image_cv)
+
+            # print(arr)
+            # img = cv_utils.calculate_corners(image_cv)
             cv2.imshow("image", img)
             key = cv2.waitKey(5)
             if key == 27:
-                cv2.destroyAllWindows()   
-    
+                cv2.destroyAllWindows()
+
+    def move_piece(self, current_img):
+        current_img = cv2.cvtColor(current_img, cv2.COLOR_BGR2GRAY)
+        if len(self.previous_images) > 0:
+            total_difference = np.zeros_like(current_img)
+            for img in self.previous_images:
+                difference = cv2.subtract(img, current_img)
+                total_difference = cv2.addWeighted(total_difference, 1, difference, 1, 0.0)
+            cv2.imshow('difference', total_difference)
+            key = cv2.waitKey(5)
+            if key == 27:
+                cv2.destroyAllWindows
+        self.previous_images.append(current_img)
+        if len(self.previous_images) > self.max_length:
+            self.previous_images.pop(0)
+
+
     def convert_ros_to_opencv(self, ros_image):
         try:
             cv_image = self.bridge.imgmsg_to_cv2(ros_image, "bgr8")
--- src/abstraction/scripts/cv_utils.py
+++ src/abstraction/scripts/cv_utils.py
@@ -1,47 +1,154 @@
 import numpy as np
 import cv2
 
-def magic(original):
-    morph_kernel = np.ones((10,10),np.uint)
-    gray = cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (3,3),0)
-    #clean = cv2.morphologyEx(blur, cv2.MORPH_OPEN, morph_kernel,iterations = 1)    
-    edges = cv2.Canny(blur,150,200,apertureSize = 3)
-    lines = cv2.HoughLinesP(edges,2,np.pi/180,125,minLineLength=20,maxLineGap=250)
-    line_img = original.copy()
-    for line in lines:
-        x1,y1,x2,y2 = line[0]
-        cv2.line(line_img,(x1,y1),(x2,y2),(0,255,0),2)
+def calculate_corners(original):
+    chessboardSize = (7,7)
+    frameSize = (1069, 599)
 
-    img = crop(line_img)
+    # Termination Criteria
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    upper_val = np.array([255, 255, 255])
-    lower_val = np.array([60, 150, 150])    
-    mask = cv2.inRange(hsv, lower_val, upper_val)
+    # Prepare object points
+    objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
+    objp[:,:2] = np.mgrid[0:chessboardSize[0], 0:chessboardSize[1]].T.reshape(-1,2)
 
-    orig = crop(original)
+    img = original.copy()
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    blurred_mask = cv2.GaussianBlur(mask, (19,19), 0)
-    #corners = cv2.goodFeaturesToTrack(blurred_mask, 100, 0.3, 60)
-    x_vals = [6.0, 116.0, 227.0, 338.0, 449.0, 559.0, 669.0, 778.0, 888.0]
-    y_vals = [6.0, 116.0, 228.0, 339.0, 450.0, 561.0, 672.0, 783.0, 894.0]
-    corners = []
-    for xval in x_vals:
-        for yval in y_vals:
-            cv2.circle(orig, (int(xval),int(yval)), 10, (0,0,255), -1)
-    
-    # for corner in corners:
-    #     x,y = corner.ravel()
-    #     cv2.circle(orig, (int(x),int(y)), 10, (0,0,255), -1)
-        #cv2.putText(orig, '{},{}'.format(x,y), (int(x) + 5, int(y) + 5), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 2)
-    
-    return orig
-    # canny = canny_edge_detection(mask)
-    # dilate = cv2.dilate(canny, (3,3), 9)
+    # Find chessboard corners
+    ret, corners = cv2.findChessboardCorners(gray, chessboardSize, None)
+
+    # If found, refine corners
+    if ret:
+        corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+        corners2 = corners2.ravel()
+        corners2 = np.reshape(corners2,(7,7,2))
+        return corners2
+    return None
+    #     return corners2
+    # else:
+    #     return None
+
+
+def get_distance(point, squares = 1):
+    point - (corners[0][1][0] - point) * squares
+
+
+def get_squares(corners, img):
+    """
+    We draw the squares of the board onto the image by taking the inside corners
+    and calculating the distance of those. after that we connect the rest of our corners
+
+
+    | xtl |   |   |   | xtr |
+    |     |   |   |   |     |
+    |     |   |   |   |     |
+    | xbr |   |   |   | xbr |
+    """
+
+    xtl = corners[0][0][0] - (corners[0][1][0] - corners[0][0][0])
+    ytl = corners[0][0][1] - (corners[1][0][1] - corners[0][0][1])
+
+    xtr = corners[0][6][0] + (corners[0][6][0] - corners[0][5][0])
+    ytr = corners[0][6][1] - (corners[1][6][1] - corners[0][6][1])
+
+    xbl = corners[6][0][0] - (corners[6][1][0] - corners[6][0][0])
+    ybl = corners[6][0][1] + (corners[6][0][1] - corners[5][0][1])
+
+    xbr = corners[6][6][0] + (corners[6][6][0] - corners[6][5][0])
+    ybr = corners[6][6][1] + (corners[6][6][1] - corners[5][6][1])
+    cv2.line(img, (xtl, ytl), (xtr,ytr), (0,0,255), 2)
+    cv2.line(img, (xtl, ytl), (xbl,ybl), (0,0,255), 2)
+    cv2.line(img, (xbl, ybl), (xbr,ybr), (0,0,255), 2)
+    cv2.line(img, (xbr, ybr), (xtr,ytr), (0,0,255), 2)
+    for i in range(len(corners)):
+        begin_vertical = None
+        begin_horizontal = None
+        # Left outer line
+        xl = corners[i][0][0] - (corners[i][1][0] - corners[i][0][0])
+        yl = (corners[i][0][1])
+        cv2.line(img, (xl, yl), (corners[i][0][0], corners[i][0][1]), (0,0,255), 2)
+
+        # Right outer line
+        xr = corners[i][6][0] + (corners[i][6][0] - corners[i][5][0])
+        yr =(corners[i][6][1])
+        cv2.line(img, (corners[i][6][0], corners[i][6][1]), (xr,yr), (0,0,255), 2)
+
+        # top outer line
+        xt = corners[0][i][0]
+        yt = corners[0][i][1] - (corners[1][i][1] - corners[0][i][1])
+        cv2.line(img, (corners[0][i][0], corners[0][i][1]), (xt,yt), (0,0,255), 2)
+
+        # bottom outer line
+        xb = corners[6][i][0]
+        yb = corners[6][i][1] + (corners[6][i][1] - corners[5][i][1])
+        cv2.line(img, (corners[6][i][0], corners[6][i][1]), (xb,yb), (0,0,255), 2)
+
+        # B ttomouter line
+
+        for j in range(len(corners[i])):
+            if begin_vertical is not None:
+                cv2.line(img, (begin_vertical[0],begin_vertical[1]), (corners[i][j][0],corners[i][j][1]), (0,0,255), 2)
+                cv2.line(img, (begin_horizontal[0], begin_horizontal[1]) ,(corners[j][i][0],corners[j][i][1]), (0,0,255), 2)
+            begin_vertical = corners[i][j]
+            begin_horizontal = corners[j][i]
+    return img
+
+
+# def magic(original):
+    # morph_kernel = np.ones((10,10),np.uint)
+    # gray = cv2.cvtColor(original,cv2.COLOR_BGR2GRAY)
+    # blur = cv2.GaussianBlur(gray, (3,3),0)
+    # #clean = cv2.morphologyEx(blur, cv2.MORPH_OPEN, morph_kernel,iterations = 1)
+    # edges = cv2.Canny(blur,150,200,apertureSize = 3)
+    # lines = cv2.HoughLinesP(edges,2,np.pi/180,125,minLineLength=20,maxLineGap=250)
+    # line_img = original.copy()
+    # for line in lines:
+    #     x1,y1,x2,y2 = line[0]
+    #     cv2.line(line_img,(x1,y1),(x2,y2),(0,255,0),2)
+
+    # img = crop(line_img)
+
+    # hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # upper_val = np.array([255, 255, 255])
+    # lower_val = np.array([60, 150, 150])
+    # mask = cv2.inRange(hsv, lower_val, upper_val)
+
     # orig = crop(original)
-    # cont = getContours(dilate, orig)
-    # return cont
+
+    # blurred_mask = cv2.GaussianBlur(mask, (19,19), 0)
+    # #corners = cv2.goodFeaturesToTrack(blurred_mask, 100, 0.3, 60)
+    # x_vals = [6.0, 116.0, 227.0, 338.0, 449.0, 559.0, 669.0, 778.0, 888.0]
+    # y_vals = [6.0, 116.0, 228.0, 339.0, 450.0, 561.0, 672.0, 783.0, 894.0]
+    # corners = []
+    # for xval in x_vals:
+    #     for yval in y_vals:
+    #         cv2.circle(orig, (int(xval),int(yval)), 10, (0,0,255), -1)
+
+    # # for corner in corners:
+    # #     x,y = corner.ravel()
+    # #     cv2.circle(orig, (int(x),int(y)), 10, (0,0,255), -1)
+    #     #cv2.putText(orig, '{},{}'.format(x,y), (int(x) + 5, int(y) + 5), cv2.FONT_HERSHEY_COMPLEX, 0.5, (0, 0, 0), 2)
+
+    # return orig
+    # # canny = canny_edge_detection(mask)
+    # # dilate = cv2.dilate(canny, (3,3), 9)
+    # # orig = crop(original)
+    # # cont = getContours(dilate, orig)
+    # # return cont
+
+
+def detect_and_draw_circles(img):
+    current_img = cv2.medianBlur(img,5)
+    current_img = cv2.cvtColor(current_img, cv2.COLOR_BGR2GRAY)
+
+    circles = cv2.HoughCircles(current_img,cv2.HOUGH_GRADIENT,1,20, param1= 75,param2=15,minRadius=8,maxRadius=15)
+    if circles is not None:
+        circles = np.round(circles [0,:]).astype("int")
+        has_found_pieces = True
+        for (x,y,radius) in circles:
+            cv2.circle(img ,(x,y),radius,(0,255,0),2)
+    return img
 
 
 def crop(img):
