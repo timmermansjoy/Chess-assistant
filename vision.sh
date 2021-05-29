455d222f93fd51251befb3c1c7d173d04401a69b
timmermansjoy <61321383+timmermansjoy@users.noreply.github.com>
2021-05-29 19:49:24 +0200

    fixed startup script

--- vision.sh
+++ vision.sh
@@ -16,13 +16,18 @@ then
 
     # start window system
     tmux new-window -t $SESSION:2 -n 'vision'
-    tmux send-keys -t 'vision' 'cd ~/Projects/catkin_ws/src/RP2021G02' C-m
+    tmux send-keys -t 'vision' 'cd ~/Projects/catkin_ws/src/RP2021G02/src/abstraction/scripts' C-m
     tmux send-keys -t 'vision' 'rosrun abstraction abstraction.py' C-m
 
       # start window system
     tmux new-window -t $SESSION:3 -n 'rosbag'
     tmux send-keys -t 'rosbag' 'cd ~/Data/bags' C-m
     tmux send-keys -t 'rosbag' 'rosbag play -l demo.bag'
+
+      # start window system
+    tmux new-window -t $SESSION:4 -n 'gui'
+    tmux send-keys -t 'gui' 'cd ~/Projects/catkin_ws/src/RP2021G02' C-m
+    tmux send-keys -t 'gui' 'rosrun abstraction window.py'
 
     tmux join-pane -v -s 2 -t 1
     tmux join-pane -h -s 3 -t 1
